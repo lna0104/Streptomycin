1b50761195888a047ebfcea20d63a212f29a8bba
Le Na <lna144199@gmail.com>
2025-04-14 13:48:37 +1000

    Chang step numbers

--- R/master.R
+++ R/master.R
@@ -1,5 +1,5 @@
 ##############################################################################
-### Step 0: Loading all libraries and scripts and define global settings ###
+### Loading all libraries and scripts and define global settings ###
 ##############################################################################
 
 library(ALJEbinf)
@@ -9,6 +9,7 @@ library(stringr)
 library(parallel)
 library(future.apply)
 library(varhandle)
+library(rentrez)
 
 library(Biostrings)
 library(pwalign)
@@ -37,6 +38,7 @@ library(quarto)
 library(NGLVieweR)
 library(htmlwidgets)
 
+
 source("R/util.R")
 source("R/bioinformatics.R")
 source("R/analyses.R")
@@ -61,6 +63,157 @@ options(nwarnings = 10000)
 
 set.seed(globsets$random_seed)
 
+########################################################################
+### Step 0: Processing reference files                              ###
+########################################################################
+# Read the mutations data from a CSV file
+muts<-read.csv("./data/reported_mutations.csv") |>
+  filter(Gene=="rpsL")
+
+# Select unique species from the mutations data and create a gene reference data frame
+selected_muts <- muts |>
+  select(Species) |>  # Select the 'Species' column
+  unique() |>  # Remove duplicate species names
+  mutate(
+    ID = NA,  # Initialize the 'ID' column with NA
+    Strain = NA,  # Initialize the 'Strain' column with NA
+    RefStrain = NA,  # Initialize the 'RefStrain' column with NA
+    Gene = "rpsL",  # Add the gene name as 'rpsL'
+    Assembly_ID = NA,  # Initialize the 'Assembly_ID' column with NA
+    NCBI_Reference_sequence = NA  # Initialize the 'NCBI_Reference_sequence' column with NA
+  )
+
+summaries<-list()
+
+# Loop through each species in the selected_muts data frame
+for (i in 1:nrow(selected_muts)){
+  species = selected_muts$Species[i]
+  cat(paste0("Working on i = ",i, " - ", species, " species\n"))
+  
+  {
+    # Search for reference genomes for the species in the NCBI assembly database
+    # Check if the species is Escherichia coli and include strain in the search term
+    if (grepl("Escherichia coli", species, ignore.case = TRUE)) {
+      term_ser <- sprintf('"%s"[ORGN] AND "MG1655"[Strain] AND ("latest refseq"[filter] AND "reference genome"[filter])', species)
+    } else {
+      term_ser <- sprintf('"%s"[ORGN] AND ("latest refseq"[filter] AND "reference genome"[filter])', species)
+    }
+    assembly_search_results <- get_summaries(db="assembly", term=term_ser)
+    # Check if any assembly search results were found
+    if (length(assembly_search_results)!=0){
+      # If found, select the first assembly result as the reference genome
+      assembly_search_final_result<-assembly_search_results[[1]]
+      selected_muts$RefStrain[i]<-TRUE
+    }else{
+      # If no reference genome was found, search for the species in general
+      term_ser<- sprintf('"%s"[ORGN] AND "latest refseq"[filter]', species)
+      assembly_search_results <- get_summaries(db="assembly", term=term_ser)
+      if (length(assembly_search_results)==0){
+        cat(paste0("Cannot find assembly data for ", species, ". Try search species name in NCBI."))
+        next
+      }
+      assembly_search_final_result<-assembly_search_results[[1]]
+      selected_muts$RefStrain[i]<-FALSE
+    }
+    # Store the assembly summary in the 'summaries' list using the assembly UID as the key
+    summaries[[assembly_search_final_result$uid]]<-assembly_search_final_result
+    selected_muts$ID[i]<-assembly_search_final_result$uid
+  }
+  # Store the assembly name in the 'Assembly_ID' column
+  selected_muts$Assembly_ID[i]<-assembly_search_final_result$assemblyname
+  
+  {
+    # Fetch links to nucleotide sequence data for the selected assembly
+    link_to_nuc <- entrez_link(dbfrom = "assembly", db = "nuccore", id = assembly_search_final_result$uid)
+    n_link_ref <- length(link_to_nuc$links$assembly_nuccore_refseq)
+    n_link <- length(link_to_nuc$links$assembly_nuccore)
+    
+    # Check if any nucleotide sequences are found
+    if (n_link == 0) {
+      cat("No nucleotide information found! Skipping\n")
+      next
+    }
+    
+    # Function to extract strain and reference sequence
+    extract_strain_info <- function(nuc_result) {
+      strain <- nuc_result$strain
+      ref_seq <- nuc_result$accessionversion
+      
+      # If the species is E. coli, force strain to be MG1655
+      if (grepl("Escherichia coli", species, ignore.case = TRUE)) {
+        strain <- "MG1655"
+      }
+      
+      return(list(strain = strain, ref_seq = ref_seq))
+    }
+    
+    # If no refseq sequences were found
+    if (n_link_ref == 0) {
+      nuc_search_results <- entrez_summary(db = "nuccore", id = link_to_nuc$links$assembly_nuccore)
+      
+      if (n_link > 1) {
+        strain_info <- extract_strain_info(nuc_search_results[[1]])
+      } else if (n_link == 1) {
+        strain_info <- extract_strain_info(nuc_search_results)
+      }
+    }
+    
+    # If refseq sequences are found
+    if (n_link_ref >= 1) {
+      nuc_search_results <- entrez_summary(db = "nuccore", id = link_to_nuc$links$assembly_nuccore_refseq)
+      
+      if (n_link_ref > 1) {
+        strain_info <- extract_strain_info(nuc_search_results[[1]])
+      } else {
+        strain_info <- extract_strain_info(nuc_search_results)
+      }
+    }
+    
+    # Assign extracted strain and reference sequence
+    selected_muts$Strain[i] <- strain_info$strain
+    selected_muts$NCBI_Reference_sequence[i] <- strain_info$ref_seq
+  }
+}
+
+#Generate the fasta name
+added_name_muts <- selected_muts |>
+  mutate(Species_name = gsub(" ", "_", Species),  # Replace spaces with underscores
+         Strain_name = gsub(" ", "_", Strain),
+         FASTA_name = paste0(Gene, "_", Species_name, "_", Strain_name)) |>
+  select(-Species_name, Strain_name) 
+
+# Save the table as a CSV file
+write.csv(added_name_muts, "data/rpsL_references.csv", row.names = FALSE)
+
+# Initialize an empty list to store the sequences from all downloads
+all_sequences <- list()
+# Loop through each genome summary in the 'summaries' list
+for (j in 1:length(summaries)) {
+  id <- names(summaries[j])
+  cat(paste0("Downloading genome for species #", j, "/", length(summaries), "\n..."))
+  
+  file_path <- download_file(summaries[[j]], dir = "output/references")
+  
+  fasta_file <- sub("\\.gz$", "", file_path)
+  sequences <- readDNAStringSet(fasta_file)
+  matching_seq <- sequences[grep("rpsL|30S ribosomal protein S12($|\\])", names(sequences))]
+  
+  if (length(matching_seq) != 0) {
+    names(matching_seq) <- added_name_muts[added_name_muts$ID == id, ]$FASTA_name
+    all_sequences <- c(all_sequences, matching_seq)
+  }else{
+    cat(paste0("No rpsL found for species ID ", id, "\n"))
+    next
+  }
+}
+# Combine all sequences into a single DNAStringSet object
+combined_sequences <- do.call(c, all_sequences)
+
+# Write the combined sequences to a new FASTA file
+writeXStringSet(combined_sequences, "data/rpsL_references.fasta")
+
+#empty working environment to keep everything clean
+rm.all.but("globsets")
 
 ########################################################################
 ### Step 1: Processing table of known STR resistance mutations       ###
@@ -101,7 +254,23 @@ muts <- mutation_list_reports |>
   fillMutationsTable(refs, seqs, coordinates) |>
   filter(!is.na(AA_mut_name_Ecoli))  # filter out "bad" entries that couldn't be mapped to E. coli
 
-write_csv(muts, "./output/muts.csv")
+compareMutationsToRef<-function(seqs, muts){
+  muts<-muts |>
+    mutate(Warning2 = NA)
+  for (k in 1:nrow(muts)){
+    seq_name<-muts$RefSeq_ID[k]
+    AA_seq<-translate(seqs[seq_name])[[1]]
+    AA_pos<-muts$AA_pos[k]
+    if (as.character(AA_seq[AA_pos]) != muts$AA_original[k] & as.character(AA_seq[AA_pos]) != muts$AA_mutation[k]){
+      muts$Warning2[k]<-"AA_original or AA_mutation inconsistent with AA_pos"
+    }
+  }
+  return(muts)
+}
+
+added_warnings_muts<-compareMutationsToRef(seqs, muts)
+
+write_csv(added_warnings_muts, "./output/muts.csv")
 
 #3 summary and plot of reported mutations
 plot_reported_mutations(muts, file_name = "./plots/reported_mutations.pdf", n_frequency = 3) # returns frequent reported mutations, positions and species
@@ -379,70 +548,9 @@ for(i in 1:nrow(networks)) {
 #empty working environment to keep everything clean:
 rm.all.but("globsets")
 
-########################################################################
-### Step 8: Analyse mutations from Yang et al.                       ###
-########################################################################
-
-# input for this step:  table of reported mutations ("./output/muts.csv")
-#                       extracted rpoB target sequences (./"output/rpoB_target_sequences.fa")
-#                       screened results for all rpoB sequences ("./output/raw_output.csv")
-#                       reported frequencies of rpoB mutations under RIF, 
-#                           from Yang et al. (2023) ("./data/Yang2023_Fig3a_data_RIF_log2FC.csv")
-#                       subtree of original tree (nwk file: "./output/subtree.nwk")
-#                       
-# output for this step: filtered results for reliable sequences("./output/filtered_output_YangOnly.csv")
-#                       summary of screening result ("./output/summary_screen_mutations.txt")
-#                       plots of target sequence statistics ("target_sequence_stats_hist.pdf" & "target_sequence_stats_pairs.pdf")
-#                       plot of predictions for each mutation across species ("./plots/mutation_screening.pdf")
-#                       plot of predictions for different classes and genera ("./plots/mutations_by_species.pdf")
-
-# load required data:
-muts <- read_csv("./output/muts.csv", show_col_types = FALSE)
-rpoB_target_sequences <- readDNAStringSet("./output/rpoB_target_sequences.fa")
-raw_output <- read_csv("./output/raw_output.csv", show_col_types = FALSE)
-subtree <- read.tree("./output/subtree.nwk")
-bacterial_taxonomy <- read_csv("./data/NCBI_taxonomy.csv", show_col_types = FALSE) #bacterial taxonomic information from NCBI
-mutation_list_Yang <- import_Yang_muts(file_name_RIF_log2FC = "./data/Yang2023_Fig3a_data_RIF_log2FC.csv",
-                                       min_log2_fc = globsets$Yang_min_log2_fc)
-
-# 1. filtering final output for reliable sequences
-filtered_output <- raw_output |>
-  process_output() |>
-  # retain only data for reliable rpoB sequences:
-  filter_output(min_seq_length = globsets$min_seq_length,
-                min_alig_score = globsets$min_alig_score,
-                max_core_dist = globsets$max_core_dist)
-
-# 2.filter to only retain mutations that have not been reported but are in Yang et al.:
-mutation_list_reports <- filter_mutations(muts,
-                                          min_n_species = globsets$min_n_species, 
-                                          min_n_studies = globsets$min_n_studies,
-                                          origin = "ONEMUT")
-
-filtered_output <- filtered_output |>
-  anti_join(mutation_list_reports, by = join_by(AA_pos_Ecoli, AA_mutation))
-
-write_csv(filtered_output,"./output/filtered_output_YangOnly.csv")
-#filtered_output <- read_csv("./output/filtered_output_YangOnly.csv", show_col_types = FALSE)
-
-#3. analysis of filtered results:
-plot_reported_vs_Yang(mutation_list_reports, mutation_list_Yang, file_name = "./plots/YangOnly/reported_vs_Yang_mutations.pdf")
-plot_mutation_screen(filtered_output, file_name = "./plots/YangOnly/mutation_screening_YangOnly.pdf")
-plot_classes_genera(filtered_output, bacterial_taxonomy, file_name= "./plots/YangOnly/mutations_by_species_YangOnly.pdf")
-summarise_mutation_screen(filtered_output,
-                          file_name = "./results/summary_mutation_screen_YangOnly.txt",
-                          subtitle = "(additional mutations from Yang et al. (2023))")
-
-#4. phylogenetic tree:
-species_output <- get_species_output(filtered_output)
-plot_subtree(subtree, species_output, bacterial_taxonomy, file_name = "./plots/YangOnly/whole_genome_tree_YangOnly.pdf")
-
-#empty working environment to keep everything clean:
-rm.all.but("globsets")
-
 
 ########################################################################
-### Step 9: Analyse all mutations (reports & from Yang et al.        ###
+### Step 8: Analyse all mutations (reports & from Yang et al.        ###
 ########################################################################
 
 # input for this step:  extracted rpoB target sequences (./"output/rpoB_target_sequences.fa")
@@ -482,7 +590,7 @@ plot_subtree(subtree, species_output, bacterial_taxonomy, file_name = "./plots/A
 rm.all.but("globsets")
 
 ########################################################################
-### Step 10: Plotting rpoB structure and sequence conservation       ###
+### Step 9: Plotting rpoB structure and sequence conservation       ###
 ########################################################################
 
 # load required data:
@@ -526,7 +634,7 @@ visualise_RpoB_structure(file_pdb = "./data/5uac_chainC.pdb",
 quarto_render("plots/rpoB_structure_embedding.qmd")
 
 ########################################################################
-### Step 11: Compile final report                                    ###
+### Step 10: Compile final report                                    ###
 ########################################################################
 
 # collating all individual summary files and rendering them as a single pdf file:
